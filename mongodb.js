c2efbad88e893a0a6e29c327eabd65b6cdcf8dd6
Dinesh Gola <dineshgola025@gmail.com>
2021-07-30 02:28:50 +0530

    Read from database

--- mongodb.js
+++ mongodb.js
@@ -1,17 +1,46 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+const {MongoClient, ObjectId, ConnectionClosedEvent} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
+const id = new ObjectId();
+//console.log(id);
+//console.log(id.getTimestamp())
 MongoClient.connect(connectionURL, { useNewUrlParser: true } , (error, client) =>{
     if(error){
         return console.log("Error in Connection");
     }
     //console.log("connected!");
     const db = client.db(databaseName);
+    // db.collection('users').findOne({name : 'rrr'} , (error, user) => {
+    //     if(error){
+    //         return console.log("error")
+    //     }
+    //     console.log(user);
+    // })
+    db.collection('users').find({name : 'rrr'}).toArray((error, user) => {
+        if(error){
+            return console.log("error")
+        }
+        console.log(user);
+    })
+
+    db.collection('users').find({name : 'rrr'}).count((error, user) => {
+        if(error){
+            return console.log("error")
+        }
+        console.log(user);
+    })
+
+});
+
+
 
     // db.collection('users').insertOne({
+    //     _id:id,
     //     name:'Dinesh',
     //     age:'28'
     // }, (error,result) => {
@@ -22,18 +51,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true } , (error, client) =
     //     console.log(result);
     // })
 
-    db.collection('users').insertMany(
-    [{
-        name : 'rrr',
-        age : 28
-    },
-    {
-        name : 'eee',
-        age:23
-    }],(error, result) => {
-        if(!error){
-            console.log(result);
-        }
-    })
-
-});
+    // db.collection('users').insertMany(
+    // [{
+    //     name : 'rrr',
+    //     age : 28
+    // },
+    // {
+    //     name : 'eee',
+    //     age:23
+    // }],(error, result) => {
+    //     if(!error){
+    //         console.log(result);
+    //     }
+    // })
