b09de1272f395f0645ef9d12c48c813c3ee8e3da
Dinesh Gola <dineshgola025@gmail.com>
2021-08-01 15:02:12 +0530

    mongoose db +  validation

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 // const mongodb = require('mongodb');
 // const MongoClient = mongodb.MongoClient;
 // const ObjectID = mongodb.ObjectID;
-const {MongoClient, ObjectId, ConnectionClosedEvent} = require('mongodb');
+const { MongoClient, ObjectId, ConnectionClosedEvent } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -9,21 +9,23 @@ const databaseName = 'task-manager';
 const id = new ObjectId();
 //console.log(id);
 //console.log(id.getTimestamp())
-MongoClient.connect(connectionURL, { useNewUrlParser: true } , (error, client) =>{
-    if(error){
-        return console.log("Error in Connection");
-    }
-    //console.log("connected!");
-    //https://docs.mongodb.com/manual/reference/operator/update/
-    const db = client.db(databaseName);
-      db.collection('users').deleteMany({name: 'rrr'} ).then((result) =>{
-        console.log(result);
-    }).catch((error) => {
-        console.log(error);
-    });
-  
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log("Error in Connection");
+  }
+  //console.log("connected!");
+  //https://docs.mongodb.com/manual/reference/operator/update/
+  const db = client.db(databaseName);
+ 
 
 });
+
+ // db.collection('users').deleteMany({ name: 'rrr' }).then((result) => {
+  //   console.log(result);
+  // }).catch((error) => {
+  //   console.log(error);
+  // });
+
 
   // db.collection('users').updateOne({name: 'rrr'},{$inc :{age: -1}} ).then((result) =>{
     //     console.log(result);
---
+++ src/db/mongoose.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid Email')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+const me = new User({
+    name: 'Dinesh',
+    email: 'dinhes@'
+})
+
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log("Error " + error)
+})
