db83321a8c3adb3c15b3db42de91b3a0fad8bc9b
Dinesh Gola <dineshgola025@gmail.com>
2021-08-04 16:03:22 +0530

    Routers code , without embedding

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+// app.use(userRouter)
+// app.use(taskRouter)
+
+// app.listen(port, () => {
+//     console.log('Server is up on port ' + port)
+// })
 
 app.post('/users', async (req, res) => {
     const user = new User(req.body)
