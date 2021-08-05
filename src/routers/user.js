38d7240811cedd7c3ae8ca183b30925ae2d64f72
Dinesh Gola <dineshgola025@gmail.com>
2021-08-06 03:32:18 +0530

    Hash password + login

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { update } = require('../models/user')
 const User = require('../models/user')
 const router = new express.Router()
 
@@ -10,6 +11,15 @@ router.post('/users', async (req, res) => {
         res.status(201).send(user)
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (e) {
+        res.status(400).send()
     }
 })
 
@@ -48,7 +58,12 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => {
+            user[update] = req.body[update]
+        })
+        await user.save()
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
         if (!user) {
             return res.status(404).send()
