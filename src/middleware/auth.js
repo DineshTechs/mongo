e74a7ab830244d7237067f925cbe49791bdbc00f
Dinesh Gola <dineshgola025@gmail.com>
2021-08-09 23:07:05 +0530

    authenticating user endpoints

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,20 +3,22 @@ const User = require('../models/user')
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer', '')
+        const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, 'Examplesrting')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
         if (!user) {
             throw new Error("No User!")
         }
+
+        req.token = token
         req.user = user
         next()
         //console.log(token)
     } catch (e) {
         res.status(401).send({ Error: 'Not authenticated!' })
     }
-    console.log('middleware')
+    //console.log('middleware')
     next()
 }
 
